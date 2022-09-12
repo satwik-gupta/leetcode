ccca08cb056730b438e2bcf86590f04f53f24aa4
Satwik Gupta <satwikgupta91@gmail.com>
2022-09-12 14:46:09 +0530

    inital commit

---
+++ binary tree inorder traversal.py
@@ -0,0 +1,10 @@
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if root is None:
+            return []
+        result=[]
+        if root:
+            result+=self.inorderTraversal(root.left)
+            result.append(root.val)
+            result+=self.inorderTraversal(root.right)
+        return result
