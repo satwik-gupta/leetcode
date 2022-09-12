f4ac5f4e330dcd8763fa08d9d264886797fc66b7
Satwik Gupta <77458910+satwik-gupta@users.noreply.github.com>
2022-09-13 04:42:34 +0530

    Time: 126 ms (7.91%), Space: 13.9 MB (99.07%) - LeetHub

---
+++ 948-bag-of-tokens/948-bag-of-tokens.py
@@ -0,0 +1,22 @@
+class Solution:
+    def bagOfTokensScore(self, tokens: List[int], power: int) -> int:
+        score = 0
+        maximumScore = 0
+        tokens.sort()
+
+        while(tokens):
+            if(power>=tokens[0]):
+                minimum = tokens.pop(0)
+                power -= minimum
+                score += 1
+                if(score>maximumScore):
+                    maximumScore = score
+            else:
+                if(score>=1):
+                    maximum = tokens.pop()
+                    power += maximum
+                    score -= 1
+                else:
+                    break
+            
+        return(maximumScore)
