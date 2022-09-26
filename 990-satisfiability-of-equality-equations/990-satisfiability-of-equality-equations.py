780c8e184f3b8b44a1f8b22c91ddd8b1f54dffbb
Satwik Gupta <77458910+satwik-gupta@users.noreply.github.com>
2022-09-26 14:51:05 +0530

    Time: 48 ms (93.97%), Space: 14.2 MB (9.51%) - LeetHub

---
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.py
@@ -0,0 +1,24 @@
+class Solution:
+    def equationsPossible(self, equations: List[str]) -> bool:
+        def find(x):
+            return x if parent[x] == x else find(parent[x])
+        # at the beginning, put each character in its own group
+        # so we will have 26 groups with one character each
+        # i.e. 'a' in group 0, 'b' in group 1, ..., 'z' in group 25
+        parent = [i for i in range(26)]
+        for e in equations:
+            if e[1] == '=':
+                # e.g. a == b
+                # then we group them together
+                # how? we use `find` function to find out the parent group of the target character index
+                # then update parent. a & b would be in group 1 (i.e. a merged into the group where b belongs to)
+                # or you can also do `parent[find(ord(e[3]) - ord('a'))] = find(ord(e[0]) - ord('a'))`
+                # i.e. b merged into the group where a belongs to
+                parent[find(ord(e[0]) - ord('a'))] = find(ord(e[3]) - ord('a'))
+        # handle != case
+        for e in equations:
+            # if two characters are not equal
+            # then which means their parent must not be equal
+            if e[1] == '!' and find(ord(e[0]) - ord('a')) == find(ord(e[3]) - ord('a')):
+                return False
+        return True
