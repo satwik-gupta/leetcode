3e6f7a03f0fd8ceec3f5172d6f9a0d9d559c826b
Satwik Gupta <77458910+satwik-gupta@users.noreply.github.com>
2022-10-03 13:36:14 +0530

    Time: 108 ms (97.52%), Space: 14 MB (89.37%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.py
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.py
@@ -4,7 +4,6 @@ class Solution:
 
         mod = 10**9 + 7
         for i in range(n - 1):
-            # start updating row from the end
             row[target - 1] = sum(row[max(0, target - 1 - k): target - 1]) % mod
             for t in range(target - 2, max(-1, target - 1 - (n - 1 - i) * k), -1):
                 row[t] = (row[t + 1] - row[t] + (row[t - k] if t - k >= 0 else 0)) % mod
