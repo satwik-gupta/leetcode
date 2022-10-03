dab0c6a1831325f24de106f94fce860f8ef16428
Satwik Gupta <77458910+satwik-gupta@users.noreply.github.com>
2022-10-03 11:43:35 +0530

    Time: 1275 ms (86.39%), Space: 24.9 MB (74.83%) - LeetHub

---
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minCost(self, s: str, nt: List[int]) -> int:
+        s=list(s)
+        i=0
+        j=1
+        x=0
+        for y in range(1,len(s)):
+            if(s[i]==s[j]):
+                if(nt[i]>=nt[j]):
+                    x+=nt[j]
+                    print(nt[j])
+                    j+=1
+                else:
+                    x+=nt[i]
+                    print(nt[i])
+                    i=j
+                    j+=1
+                    
+            else:
+                i=j
+                j+=1
+        return x
