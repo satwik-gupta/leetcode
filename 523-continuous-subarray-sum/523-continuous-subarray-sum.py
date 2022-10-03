61a3da252af3a46c4ab593bb6833d171fd424bff
Satwik Gupta <77458910+satwik-gupta@users.noreply.github.com>
2022-10-03 13:30:56 +0530

    Time: 1276 ms (78.34%), Space: 31.4 MB (90.27%) - LeetHub

--- 523-continuous-subarray-sum/523-continuous-subarray-sum.py
+++ 523-continuous-subarray-sum/523-continuous-subarray-sum.py
@@ -1,6 +1,7 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
         d={0:-1}
+        ans=0
         if(len(nums)==1):
             return False
         for i in range(1,len(nums)):
@@ -8,9 +9,11 @@ class Solution:
         for i in range(len(nums)):
             nums[i]=nums[i]%k
         for i in range(len(nums)):
-            if nums[i] not in d:
+            if nums[i] in d:
+                if((i-d[nums[i]])>1):
+                    return True
+                else:
+                    i+=1
+            else:
                 d[nums[i]]=i
-        for i in range(len(nums)):
-            if i-d[nums[i]]>1:
-                return True
         return False
