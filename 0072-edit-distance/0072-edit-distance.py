dcbad384cf8046b86763dedf6f06d1fba9e9ec39
Satwik Gupta <77458910+satwik-gupta@users.noreply.github.com>
2022-11-01 14:43:01 +0530

    Time: 222 ms (71.25%), Space: 17.6 MB (60.38%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minDistance(self, s1: str, s2: str) -> int:
+        m=len(s1)
+        n=len(s2)
+        dp=[[0]*(n+1) for _ in range(m+1)]
+        for i in range(m+1):
+            for j in range(n+1):
+                if(i==0 and j==0):
+                    dp[i][j]=0
+                elif(i==0):
+                    dp[i][j]=j
+                elif(j==0):
+                    dp[i][j]=i
+                elif(s1[i-1]==s2[j-1]):
+                    dp[i][j]=dp[i-1][j-1]
+                else:
+                    dp[i][j]=1+ min(dp[i][j-1],dp[i-1][j],dp[i-1][j-1])
+        return dp[m][n]
