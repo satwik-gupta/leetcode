93499c904eb37b36e746d4cf7f1f7a0ef27cf9e2
Satwik Gupta <77458910+satwik-gupta@users.noreply.github.com>
2022-11-06 01:00:00 +0530

    Time: 89 ms (99.63%), Space: 14.4 MB (97.87%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.py
+++ 0212-word-search-ii/0212-word-search-ii.py
@@ -1,79 +1,73 @@
-class Trie:
-    def __init__(self, words=[]):
-        self.trie = {}
-        for w in words: self.insert(w)
-
-    def insert(self, word):
-        t = self.trie
-        for w in word:
-            if w not in t:
-                t[w] = {}
-            t = t[w]
-        t['#'] = '#'
-
-    def search(self, word):
-        t = self.trie
-        for w in word:
-            if w not in t:
-                return False
-            t = t[w]
-        if '#' in t:
-            return True
-        return False
-
-    def starts(self, prefix):
-        t = self.trie
-        for w in prefix:
-            if w not in t:
-                return False
-            t = t[w]
-        return True
-    
-    def remove(self, word):
-        t = self.trie
-        nodes = []
-        for w in word:
-            if w not in t: return
-            t = t[w]
-            nodes.append((t,w))
-
-        if '#' in t:
-            p = '#'
-            for n,w in nodes[::-1]:
-                if len(n[p]) == 0 or p == '#' : del n[p]
-                p = w                
 class Solution:
-    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
-        
-        m, n = len(board), len(board[0])
-        trie = Trie(words)                   # trie is initialized with a list of words
-        seen = set()                         # found words are being collected into a set
-                
-        # generator that yields (!) adjacent cells
-        def adj(x, y):
-            for i,j in [(0,-1),(0,1),(-1,0),(1,0)]:
-                if 0 <= x+i < m and 0 <= y+j < n:
-                    yield (x+i,y+j)
-        
-        # DFS search with prefix 'p', last cell (x,y) on board 'b'
-        def dfs(p, b, x, y):          
-            
-            ch, b[x][y] = b[x][y], "#"        # [1] mark used cell and save board state
-            
-            if trie.search(p):               
-                seen.add(p)                   # [2] mark word as found and
-                trie.remove(p)                #     no longer search for it
+    def findWords(self, board, words):
 
-            for i,j in adj(x, y):             # [3] iterate over adjacent cells
-                if b[i][j] != "#":            #     which are still unused
-                    pp = p + b[i][j]          #     and extend the word
-                    if trie.starts(pp):       # [4] if the prefix exists in the trie,
-                        dfs(pp, b, i, j)      #     we should check this branch
-            
-            b[x][y] = ch                      # [5] restore board state
-        
-        for i in range(m):                    # DFS procedure is initialized starting
-            for j in range(n):                # from all possible cells (i,j)
-                dfs(board[i][j], board, i, j)
-        
-        return seen
+        m, n = len(board), len(board[0])
+        dic = defaultdict(set)
+
+        for i in range(m):
+            for j in range(n):
+                dic[board[i][j]].add((i, j))
+
+        results = deque()
+
+        word, lngth, word_isReversed = '', 0, False
+        def dfs(cord, indx):
+            if indx == lngth: 
+                if word_isReversed:
+                    results.append(word[::-1])
+                else:
+                    results.append(word)
+                return True
+
+            ch = word[indx]
+            i, j = cord
+            for cand in [(i - 1, j), (i, j - 1), (i, j + 1), (i + 1, j)]:
+                if cand in dic[ch]:
+                    dic[ch].remove(cand)
+                    flag = dfs(cand, indx + 1)
+                    dic[ch].add(cand)
+                    if flag: return True
+
+            return False
+
+
+        ref = set()
+        for i in range(m):
+            for j in range(n - 1):
+                ref.add(board[i][j] + board[i][j + 1])
+        for j in range(n):
+            for i in range(m - 1):
+                ref.add(board[i][j] + board[i + 1][j])
+
+        #len_dic = len(dic)
+        def check(word):
+            for i in range(len(word) - 1):
+                if word[i] + word[i + 1] not in ref:
+                    if word[i + 1] + word[i] not in ref:
+                        return False
+            # wordCount = Counter(word)
+            # if len(wordCount) > len_dic:
+            #     return False
+            # for ch, count in wordCount.items():
+            #     if len(dic[ch]) < count:
+            #         return False
+            return True
+
+
+        for w in words:
+            if check(w):
+                if w[:4] == w[0]*4 or len(dic[w[-1]]) < len(dic[w[0]]):
+                    word = w[::-1]
+                    word_isReversed = True
+                else:
+                    word = w
+                    if word_isReversed: word_isReversed = False
+
+                lngth = len(word)
+                for cord in list(dic[word[0]]):
+                    dic[word[0]].remove(cord)
+                    flag = dfs(cord, 1)
+                    dic[word[0]].add(cord)
+                    if flag: break
+
+        return results
