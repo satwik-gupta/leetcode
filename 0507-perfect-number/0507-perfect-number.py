bbaf28eebba8cdfbb2f71235b689cb0d9708f1a7
Satwik Gupta <77458910+satwik-gupta@users.noreply.github.com>
2022-11-15 14:04:02 +0530

    Time: 83 ms (41.49%), Space: 13.9 MB (59.01%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.py
+++ 0507-perfect-number/0507-perfect-number.py
@@ -6,7 +6,8 @@ class Solution:
         for i in range(2,int(num**0.5)+1):
             if(num%i==0):
                 n+=i
-                n+=num//i
+                if(num//i!=i):
+                    n+=num//i
         if(n==num):
             return True
         else: return False
