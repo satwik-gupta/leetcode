928aa42747ad5202d4eea6db7c88ff0d85800d1c
Satwik Gupta <77458910+satwik-gupta@users.noreply.github.com>
2022-11-15 13:58:59 +0530

    Time: 77 ms (49.51%), Space: 13.8 MB (97.72%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def checkPerfectNumber(self, num: int) -> bool:
+        n=1
+        if(num==1):
+            return False
+        for i in range(2,int(num**0.5)+1):
+            if(num%i==0):
+                n+=i
+                n+=num//i
+        if(n==num):
+            return True
+        else: return False
