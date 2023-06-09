72c3f6776564efd646e192a506ab3e21230b4c9e
Satwik Gupta <77458910+satwik-gupta@users.noreply.github.com>
2023-06-09 23:11:37 +0530

    Time: 68 ms (63.73%), Space: 16.4 MB (67.28%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -9,13 +9,23 @@ class Solution:
 #                 return cost[n]
 #             return cost[n] + min(x(cost,n-1),x(cost,n-2))
 #         return min(x(cost,n-1),x(cost,n-2))
-        memo={}
-        memo[0]=cost[0]
-        memo[1]=cost[1]
-        def mincost(cost,n):
-            if n in memo:
-                return memo[n]
-            else:
-                memo[n]=cost[n]+min(mincost(cost,n-1),mincost(cost,n-2))
-                return memo[n]
-        return min(mincost(cost,n-1),mincost(cost,n-2))
+        # memo={}
+        # memo[0]=cost[0]
+        # memo[1]=cost[1]
+        # def mincost(cost,n):
+        #     if n in memo:
+        #         return memo[n]
+        #     else:
+        #         memo[n]=cost[n]+min(mincost(cost,n-1),mincost(cost,n-2))
+        #         return memo[n]
+        # return min(mincost(cost,n-1),mincost(cost,n-2))
+        if(n==0):
+            return cost[0]
+        if(n==1):
+            return cost[1]
+        dp=[0]*(n+1)
+        dp[0]=cost[0]
+        dp[1]=cost[1]
+        for i in range(2,n):
+            dp[i]=cost[i]+min(dp[i-1],dp[i-2])
+        return min(dp[n-1],dp[n-2])
