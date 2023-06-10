1fbeb164cde9305a4054cfe0a10bd8a6a47c32b1
Satwik Gupta <77458910+satwik-gupta@users.noreply.github.com>
2023-06-10 10:58:28 +0530

    Time: 45 ms (54.31%), Space: 16.3 MB (55.35%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -16,14 +16,21 @@ class Solution:
             return nums[0]
         if(n==1):
             return max(nums[0],nums[1])
-        memo={}
-        memo[0]=nums[0]
-        memo[1]=max(nums[0],nums[1])
+#         memo={}
+#         memo[0]=nums[0]
+#         memo[1]=max(nums[0],nums[1])
         
-        def c(nums,n):
-            if n in memo:
-                return memo[n]
-            else:
-                memo[n]=max(nums[n]+c(nums,n-2),c(nums,n-1))
-                return memo[n]
-        return max(c(nums,n),c(nums,n-1))
+#         def c(nums,n):
+#             if n in memo:
+#                 return memo[n]
+#             else:
+#                 memo[n]=max(nums[n]+c(nums,n-2),c(nums,n-1))
+#                 return memo[n]
+#         return max(c(nums,n),c(nums,n-1))
+        dp=[0]*(n+1)
+        dp[0]=nums[0]
+        dp[1]=max(nums[0],nums[1])
+        for i in range(2,n+1):
+            dp[i]=max(nums[i]+dp[i-2],dp[i-1])
+        return max(dp[n],dp[n-1])
+    
