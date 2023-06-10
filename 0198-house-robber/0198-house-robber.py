a0b8f85058a9ae39243b624f05c7dbd055e909d8
Satwik Gupta <77458910+satwik-gupta@users.noreply.github.com>
2023-06-10 10:54:03 +0530

    Time: 43 ms (62.20%), Space: 16.2 MB (55.35%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,29 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n=len(nums)-1
+        # if(n==0):
+        #     return nums[0]
+        # if(n==1):
+        #     return max(nums[0],nums[1])
+        # def c(nums,n):
+        #     if(n==0):
+        #         return nums[0]
+        #     if(n==1):
+        #         return max(nums[1],nums[0])
+        #     return max(nums[n]+c(nums,n-2),c(nums,n-1))
+        # return max(c(nums,n-1),c(nums,n))
+        if(n==0):
+            return nums[0]
+        if(n==1):
+            return max(nums[0],nums[1])
+        memo={}
+        memo[0]=nums[0]
+        memo[1]=max(nums[0],nums[1])
+        
+        def c(nums,n):
+            if n in memo:
+                return memo[n]
+            else:
+                memo[n]=max(nums[n]+c(nums,n-2),c(nums,n-1))
+                return memo[n]
+        return max(c(nums,n),c(nums,n-1))
