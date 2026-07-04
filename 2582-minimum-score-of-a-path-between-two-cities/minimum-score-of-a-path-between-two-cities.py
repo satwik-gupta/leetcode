ab4cea32eae55082c84a9488229c545d2a3c2e76
Satwik Gupta <77458910+satwik-gupta@users.noreply.github.com>
2026-07-04 20:34:06 +0530

    Time: 162 ms (64.22%) | Memory: 68.2 MB (84.89%) - LeetSync

---
+++ 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.py
@@ -0,0 +1,26 @@
+class Solution:
+    def minScore(self, n: int, roads: List[List[int]]) -> int:
+        graph=[[] for i in range(n+1)]
+        for a,b,distance in roads:
+            graph[a].append((b,distance))
+            graph[b].append((a,distance))
+
+        visited=[False]*(n+1)
+        queue=deque([1])
+        visited[1]=True
+
+        answer=float("inf")
+
+        while queue:
+            city =queue.popleft()
+            for next_city,distance in graph[city]:
+                answer=min(answer,distance)
+
+                if not visited[next_city]:
+                    answer=min(answer,distance)
+
+                    if not visited[next_city]:
+                        visited[next_city]=True
+                        queue.append(next_city)
+
+        return answer
