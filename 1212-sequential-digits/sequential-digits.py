c5e68f1fbd6d4131a3659210504469d9e332c872
Satwik Gupta <77458910+satwik-gupta@users.noreply.github.com>
2026-07-13 21:17:56 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (64.31%) - LeetSync

--- 1212-sequential-digits/sequential-digits.py
+++ 1212-sequential-digits/sequential-digits.py
@@ -1,15 +1,17 @@
+from typing import List
+
 class Solution:
+    # 1. Precompute all 36 sequential numbers at the class level
+    ALL_NUMS = []
+    for i in range(1, 9):
+        num = i
+        for j in range(i + 1, 10):
+            num = num * 10 + j
+            ALL_NUMS.append(num)
+            
+    # 2. Sort them once
+    ALL_NUMS.sort()
+
     def sequentialDigits(self, low: int, high: int) -> List[int]:
-        ans = []
-
-        s = "123456789"
-        l = str(low)
-        h = str(high)
-
-        for length in range(len(l), len(h) + 1):
-            for start in range(0, 10 - length):
-                num = int(s[start:start + length])
-                if low <= num <= high:
-                    ans.append(num)
-
-        return ans
+        # 3. List comprehension running at C-speed to filter the answers
+        return [x for x in self.ALL_NUMS if low <= x <= high]
