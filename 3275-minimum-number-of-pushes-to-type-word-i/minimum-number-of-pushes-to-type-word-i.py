ffc19ca94c95d2535470dc40372658b2efb50e2a
Satwik Gupta <77458910+satwik-gupta@users.noreply.github.com>
2026-07-30 14:47:28 +0530

    Time: 1 ms (18.24%) | Memory: 19.3 MB (48.74%) - LeetSync

--- 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.py
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.py
@@ -3,8 +3,8 @@ class Solution:
         n=len(word)
         if n<=8:
             return n
-        count=8
-        for i in range(8,n):
+        count=0
+        for i in range(0,n):
             mul=math.ceil((i+1)/8)
             count+=1*mul
         return count
