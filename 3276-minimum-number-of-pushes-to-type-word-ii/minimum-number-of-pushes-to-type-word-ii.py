3ea927c7a2b08ec1805d755cc4c45b088d5e7450
Satwik Gupta <77458910+satwik-gupta@users.noreply.github.com>
2026-07-31 23:08:53 +0530

    Time: 96 ms (76.59%) | Memory: 20.3 MB (9.03%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.py
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.py
@@ -1,14 +1,19 @@
 class Solution:
     def minimumPushes(self, word: str) -> int:
-        count={}
-        ans=0
-        for i in word:
-            if i not in count:
-                count[i]=1
-            else:
-                count[i]+=1
-        sorted_values = sorted(count.values(), reverse=True)
-        for i in range(len(sorted_values)):
-            counter=math.ceil((i+1)/8)
-            ans+=counter*sorted_values[i]
-        return ans
+        # Frequency map to store count of each letter
+        frequency_map = Counter(word)
+
+        # Priority queue to store frequencies in descending order
+        frequency_queue = [-freq for freq in frequency_map.values()]
+        heapq.heapify(frequency_queue)
+
+        total_pushes = 0
+        index = 0
+
+        # Calculate total number of presses
+        while frequency_queue:
+            total_pushes += (1 + (index // 8)) * (
+                -heapq.heappop(frequency_queue)
+            )
+            index += 1
+        return total_pushes
