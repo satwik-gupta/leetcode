aadf71a061630d734f8f0e96b670f5ce8cad3dc3
Satwik Gupta <77458910+satwik-gupta@users.noreply.github.com>
2026-08-31 12:26:05 +0530

    Time: 91 ms (37.53%) | Memory: 62.8 MB (93.20%) - LeetSync

---
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def nodesBetweenCriticalPoints(self, head: Optional[ListNode]) -> List[int]:
+        first = prev = None
+        a = b = c = 0
+        min_dist = inf
+        i = 1
+        res = [-1, -1]
+        while head:
+            a, b, c = b, c, head.val
+            if a and b and c and (a > b < c or a < b > c):
+                if first is None:
+                    first = i
+                else:
+                    min_dist = min(min_dist, i - prev)
+                    res = [min_dist, i - first]
+                prev = i
+            i += 1
+            head = head.next
+        return res
