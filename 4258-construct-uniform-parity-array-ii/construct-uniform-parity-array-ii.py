b196134a44ce1c61a77eb59603ecd25cd53eb430
Satwik Gupta <77458910+satwik-gupta@users.noreply.github.com>
2026-09-03 22:03:23 +0530

    Time: 56 ms (63.41%) | Memory: 35 MB (95.12%) - LeetSync

---
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.py
@@ -0,0 +1,15 @@
+class Solution:
+    def uniformArray(self, nums1: list[int]) -> bool:
+        nums1.sort()
+        odd=False
+        lowestodd=10**5 -1
+        for i in range(len(nums1)):
+            if(nums1[i]%2==1):
+                odd=True
+                lowestodd=nums1[i]
+                break
+        if(odd and lowestodd==nums1[0]):
+            return True
+        if not odd:
+            return True
+        return False
